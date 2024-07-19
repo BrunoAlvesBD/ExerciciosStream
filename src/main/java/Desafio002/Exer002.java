36ce817113520f7b9a39e48bb49f8138cdf5ab5a
BrunoAlvesBD <bruno.dona@al.infnet.edu.br>
2024-07-19 17:23:52 -0300

    atualizando desafio 02

--- src/main/java/Desafio002/Exer002.java
+++ src/main/java/Desafio002/Exer002.java
@@ -9,7 +9,8 @@ public class Exer002 {
 
     public static void main(String[] args) {
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
-        //utilizando stream.filter para separar os valores pares e reduce com Method reference para somar.
+        //utilizando stream.filter para separar os valores pares
+        // e reduce com Method reference para somar.
         int result = (Integer) numeros.stream().filter((n) -> {
             return n % 2 == 0;
         }).reduce(0, Integer::sum);
